94b37db76d3557b86399c128d2a0425626ae8de9
BesnikBegunca <93323159+BesnikBegunca@users.noreply.github.com>
2022-02-26 15:35:06 +0000

    Add files via upload

---
+++ finale/Eco.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="eco.css">
+	<title></title>
+</head>
+<body>
+	<h1>Kits</h1>
+	<div class="kejt">
+		<div class="kit1" id="kit">
+		
+		</div>
+		<div class="kit2" id="kit">
+		
+		</div>
+		<div class="kit3" id="kit">
+		
+		</div>
+		<div class="kit4" id="kit">
+		
+		</div>
+		<div class="kit5" id="kit">
+		
+		</div>
+			<div class="kit6" id="kit">
+		
+		</div>
+		<div class="kit7" id="kit">
+		
+		</div>
+		<div class="kit8" id="kit">
+		
+		</div>
+		<div class="kit9" id="kit">
+		
+		</div>
+	</div>
+	
+
+
+</body>
+</html>
